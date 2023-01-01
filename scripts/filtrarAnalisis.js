d62e6ec81b03e0af6809d1dec295f47ca8792c1f
Mica Rzeznik <mica.rzeznik@gmail.com>
2023-01-01 20:30:52 -0300

    con sugerencias

--- scripts/filtrarAnalisis.js
+++ scripts/filtrarAnalisis.js
@@ -1,12 +1,11 @@
 const analisis = []
-
+const filtro = document.querySelector("input#filtrarAnalisis")
 //El precio final es la unidad bioquímica por el valor de esta que es 200 pesos
 
 fetch('../json/analisis.json')
     .then((response)=>data=response.json())
     .then((data) => analisis.push(...data))
     .then(() => listarAnalisisHTML(analisis))
-    .then(()=> filtrarAnalisis())
 
 function listarAnalisisHTML(array) {
     const tabla = document.querySelector("tbody")
@@ -20,9 +19,10 @@ function listarAnalisisHTML(array) {
         })
     document.getElementById("mensajeError").classList.add('mensajeError')
 }
-const filtro = document.querySelector("input#filtrarAnalisis")
-function filtrarAnalisis() {
+
+function filtrar() {
     let encontrados = analisis.filter((analisis)=> analisis.ensayo.toUpperCase().includes(filtro.value.toUpperCase().trim()))
     encontrados.length > 0 ? listarAnalisisHTML(encontrados) : document.getElementById("mensajeError").classList.remove('mensajeError')
 }
 
+filtro.addEventListener("search", filtrar)
